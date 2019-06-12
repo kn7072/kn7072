3d6eb43e25334f99d79610cdcaf8bf3bbef15177
kn7072 <kn7072@yandex.ru>
2019-06-12 19:19:32 +0300

    неличные формы - начало

---
+++ EnglishSimulate/Project/Vocabulary/analisis.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import json
+from copy import deepcopy
+
+path_to_json_words = "words.json"
+with open(path_to_json_words, encoding="utf-8") as f:
+    obj_words = json.loads(f.read())
+
+modificate_obj = deepcopy(obj_words)
+
+name_groups = ["Инфинитив с to после глаголов", "Инфинитив"]
+path_to_words = r"НеличныеФормы\Инфинитив\ПослеГлаголов\слова.txt"
+list_word = [word_i.strip() for word_i in open(path_to_words, encoding="utf-8")]
+
+temp = {
+        "transcription": "",
+        "examples": [],
+        "antonyms": [],
+        "translate": "",
+        "comment": [],
+        "mnemonic": [],
+        "synonyms": [],
+        "grups": ["all_words"].extend(name_groups)
+    }
+
+
+def analisis():
+    for word_i in list_word:
+        find_word = obj_words.get(word_i)
+        if find_word:
+            modificate_obj[word_i]["grups"].extend(name_groups)
+        else:
+            print("Не обнаружено слово %s" % word_i)
+            modificate_obj[word_i] = temp
+
+
+def crate_json_file():
+    data_words = json.dumps(modificate_obj, ensure_ascii=False, indent=4)
+    with open(r"words.json", mode="w", encoding="utf-8") as f:
+        f.write(data_words)
+    print()
+
+analisis()
+print()
+crate_json_file()
