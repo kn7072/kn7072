f836b3670ddadc07f099ca727c22b80def5aee02
stepan <stepan@stepan-HP-ENVY-dv7-Notebook-PC>
2021-03-02 12:04:01 +0300

    доработка

--- ANKI/TelegramBot/start_telegram_bot.py
+++ ANKI/TelegramBot/start_telegram_bot.py
@@ -2,10 +2,25 @@
 from config_bot import token, path_file_not_learn
 import telebot
 import os
-from common import sound, parse_file, play_sound, prepare_garibjan, send_message_from_bot
+from common import sound, parse_file, play_sound, prepare_garibjan, send_message_from_bot, prepare_galagoliya
 
 bot = telebot.TeleBot(token)
 mnemo_garibjan = prepare_garibjan()
+mnemo_galagoliya = prepare_galagoliya()
+
+
+def get_mnemo_galagoliya(word):
+    list_temp = []
+    delimetr = "#"*30
+    result = ""
+    for i in mnemo_galagoliya:
+        block_i = "\n".join(i)
+        if word in block_i:
+            list_temp.append(block_i)
+
+    if list_temp:
+        result = delimetr.join(list_temp)
+    return result    
 
 
 @bot.message_handler(content_types=['text'])
@@ -28,8 +43,10 @@ def get_text_messages(message):
             f.write(word_i + "\n")
     elif message.text.endswith("_m"):
         word_i = message.text.replace("_m", "")
-        garibjan = mnemo_garibjan.get(word_i, "-")
-        send_message_from_bot(garibjan)
+        garibjan = mnemo_garibjan.get(word_i, "")
+        galagoliya = get_mnemo_galagoliya(word_i)
+        
+        send_message_from_bot(garibjan + "\n" + galagoliya)
 
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
