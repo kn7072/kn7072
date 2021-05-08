c606d374ca347c74b9740cbf48d5d58f2e4cc126
stapan <stapan@stapan-HP-ENVY-dv7-Notebook-PC>
2021-05-08 13:05:10 +0300

    поправил ошибки в скрипте

--- ANKI/TelegramBot/start.bash
+++ ANKI/TelegramBot/start.bash
@@ -5,11 +5,11 @@ python3 sound_word.py &
 
 echo "PID родительского процесса $$"
 echo "PID sound_word.py $!"
-SOUND_WORD_PID = $!
+SOUND_WORD_PID=$!
 
 python3 server.py &
 echo "PID server.py $!"
-SERVER_PID = $!
+SERVER_PID=$!
 
 trap "kill -SIGTERM $SOUND_WORD_PID $SERVER_PID" SIGINT SIGTERM
 wait
