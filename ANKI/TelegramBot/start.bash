858a9b9cab9c1262e31e944027b291e23a01a1a2
stapan <stapan@stapan-HP-ENVY-dv7-Notebook-PC>
2021-05-08 12:32:29 +0300

    файл запуска

---
+++ ANKI/TelegramBot/start.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python3 sound_word.py &
+
+echo "PID родительского процесса $$"
+echo "PID sound_word.py $!"
+SOUND_WORD_PID = $!
+
+python3 server.py &
+echo "PID server.py $!"
+SERVER_PID = $!
+
+trap "kill -SIGTERM $SOUND_WORD_PID $SERVER_PID" SIGINT SIGTERM
+wait
