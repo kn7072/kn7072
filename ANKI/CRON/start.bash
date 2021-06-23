40da2ee202886f804c2933c80b2e70b058c5dc9d
stapan <stapan@stapan-HP-ENVY-dv7-Notebook-PC>
2021-06-23 17:09:06 +0300

    поправил скрипт

--- ANKI/CRON/start.bash
+++ ANKI/CRON/start.bash
@@ -7,12 +7,11 @@ pid_player=$(ps -C ${player_name} -o pid=)
 if (($pid_player)); then
     echo "${player_name} уже запущена, pid=$pid_player"
     echo "Выполняем команду ${player_name} $1"
-    clementine $1
+    clementine $1 &
 else 
-    clementine  # запускаем без параметров
-    sleep 5
     echo "Выполняем команду ${player_name} $1"
-    clementine $1 # выполняем команду
+    clementine $1 & # выполняем команду
+    echo "Запустили $player_name, pid=$!"
 fi 
 
 
