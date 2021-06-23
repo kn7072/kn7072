a3da41c793aceb805f5dc800ddb632623be843a0
stapan <stapan@stapan-HP-ENVY-dv7-Notebook-PC>
2021-06-23 18:14:06 +0300

    отладка

--- ANKI/CRON/start.bash
+++ ANKI/CRON/start.bash
@@ -3,15 +3,16 @@ export DISPLAY=:0
 
 player_name="clementine"
 pid_player=$(ps -C ${player_name} -o pid=)
+echo "Start PID ${pid_player}"
 
 if (($pid_player)); then
     echo "${player_name} уже запущена, pid=$pid_player"
     echo "Выполняем команду ${player_name} $1"
-    nohup clementine $1 &
+    clementine $1 &
 else 
-    nohup clementine & 
+    clementine & 
     echo "Выполняем команду ${player_name} $1"
-    nohup clementine $1 & # выполняем команду
+    clementine $1 & # выполняем команду
     echo "Запустили $player_name, pid=$!"
 fi 
 
