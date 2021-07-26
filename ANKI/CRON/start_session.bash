d154002937eb73e4a5c024fe8e2963f66b6d703d
stapan <stapan@stapan-HP-ENVY-dv7-Notebook-PC>
2021-07-26 16:22:52 +0300

    доработал скрипт чтобы останавливать и запускать clementine

--- ANKI/CRON/start_session.bash
+++ ANKI/CRON/start_session.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ -f ~/LOGS/.Xdbus ]
-then
+if [ -f ~/LOGS/.Xdbus ]; then
     # source ~/LOGS/.Xdbus;
-    source ~/LOGS/.Xdbus; dbus-send --session --type=method_call --dest=org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+    # Pause, Play, Stop, PlayPause
+    source ~/LOGS/.Xdbus; dbus-send --session --type=method_call --dest=org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.$1
 else
     echo "File doesn't exist"
 fi
