1a62d1772245f4ac4fdf010acc632b2ba47e1a98
stapan <stapan@stapan-HP-ENVY-dv7-Notebook-PC>
2021-08-01 18:31:15 +0300

    отладочный комментарий

--- ANKI/CRON/start_session.bash
+++ ANKI/CRON/start_session.bash
@@ -2,6 +2,7 @@
 if [ -f ~/LOGS/.Xdbus ]; then
     # source ~/LOGS/.Xdbus;
     # Pause, Play, Stop, PlayPause
+	echo "$1"
     source ~/LOGS/.Xdbus; dbus-send --session --type=method_call --dest=org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.$1
 else
     echo "File doesn't exist"
