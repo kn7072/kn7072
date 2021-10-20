26af5d179a5cb69a978797966127c97d2e418413
stapan <stapan@stapan-HP-ENVY-dv7-Notebook-PC>
2021-10-20 17:09:26 +0300

    увеличил время работы скрипта

--- ANKI/TelegramBot/config_bot.py
+++ ANKI/TelegramBot/config_bot.py
@@ -42,20 +42,20 @@ if not os.path.isdir(path_to_save_reports):
 name_base = "words_of_day.db"
 separate = "|#|"
 
-wait_sound = 900 #  480 600 900 1200
+wait_sound = 150  # 480 600 900 1200
 time_sound_pause = 5
 count_sound = 2  # 2
 
 schedule = {
-    "Monday": [{"start": "9:10", "stop": "13:10"}
+    "Monday": [{"start": "9:10", "stop": "15:10"}
                # {"start": "21:10", "stop": "22:10"}
                ],
-    "Tuesday": [{"start": "9:10", "stop": "13:10"}], 
-    "Wednesday": [{"start": "9:10", "stop": "13:10"}],
-    "Thursday": [{"start": "9:10", "stop": "13:10"}],
-    "Friday": [{"start": "9:10", "stop": "13:10"}],
-    "Saturday": [{"start": "11:40", "stop": "13:00"}],
-    "Sunday": [{"start": "11:40", "stop": "13:00"}
+    "Tuesday": [{"start": "9:10", "stop": "15:10"}],
+    "Wednesday": [{"start": "9:10", "stop": "15:10"}],
+    "Thursday": [{"start": "9:10", "stop": "15:10"}],
+    "Friday": [{"start": "9:10", "stop": "15:10"}],
+    "Saturday": [{"start": "11:40", "stop": "15:00"}],
+    "Sunday": [{"start": "11:40", "stop": "15:00"}
                # {"start": "15:20", "stop": "20:30"}
                ]
 }
