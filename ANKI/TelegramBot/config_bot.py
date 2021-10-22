87d776ce0054f5d8963de9f6d981306c544f415c
stapan <stapan@stapan-HP-ENVY-dv7-Notebook-PC>
2021-10-22 12:30:13 +0300

    убрал пинг из серсисов

--- ANKI/TelegramBot/config_bot.py
+++ ANKI/TelegramBot/config_bot.py
@@ -47,15 +47,15 @@ time_sound_pause = 5
 count_sound = 2  # 2
 
 schedule = {
-    "Monday": [{"start": "9:10", "stop": "15:10"}
+    "Monday": [{"start": "9:10", "stop": "13:10"}
                # {"start": "21:10", "stop": "22:10"}
                ],
-    "Tuesday": [{"start": "9:10", "stop": "15:10"}],
-    "Wednesday": [{"start": "9:10", "stop": "15:10"}],
-    "Thursday": [{"start": "9:10", "stop": "15:10"}],
-    "Friday": [{"start": "9:10", "stop": "15:10"}],
-    "Saturday": [{"start": "11:40", "stop": "15:00"}],
-    "Sunday": [{"start": "11:40", "stop": "15:00"}
+    "Tuesday": [{"start": "9:10", "stop": "13:10"}],
+    "Wednesday": [{"start": "9:10", "stop": "13:10"}],
+    "Thursday": [{"start": "9:10", "stop": "13:10"}],
+    "Friday": [{"start": "9:10", "stop": "13:10"}],
+    "Saturday": [{"start": "11:40", "stop": "13:00"}],
+    "Sunday": [{"start": "11:40", "stop": "13:00"}
                # {"start": "15:20", "stop": "20:30"}
                ]
 }
