437238debf20fcab8e91804378bb8b30758ae4de
stapan <stapan@stapan-HP-ENVY-dv7-Notebook-PC>
2021-11-05 12:17:16 +0300

    прототип

--- patterns/my_patterns/flyweight.py
+++ patterns/my_patterns/flyweight.py
@@ -2,4 +2,55 @@
 """Паттерн легковес."""
 from __future__ import annotations
 
+from weakref import WeakValueDictionary
 
+
+class FlyweightColor:
+    """Хранит общую часть состояния(внешнее состояние)."""
+
+    def __init__(self: FlyweightColor, color_name: str) -> None:
+        """Инициализирует экземпляр."""
+        self.color_name = color_name
+
+    def __str___(self: FlyweightColor) -> str:
+        """Возвращает строковое представление объекта."""
+        return self.color_name
+
+
+class FlyweightFactory:
+    """Фабрика легковесов."""
+
+    __slots__ = ()
+    _colors = WeakValueDictionary()
+
+    @classmethod
+    def get_color(cls: FlyweightFactory, color_name: str) -> FlyweightColor:
+        """Возвращает объект цвета(внешнего состояния)."""
+        object_color = cls._colors.get(color_name)
+        if object_color is None:
+            object_color = FlyweightColor(color_name)
+            cls._colors[color_name] = object_color
+        return object_color
+
+
+class PlaceMark:
+    """Объект обладающий внутренним и внешнем состоянием(внешнее состояние храним отдельн)."""
+
+    def __init__(self: PlaceMark, latitude: float, longitude: float, color_name: str) -> None:
+        """Инициализирует экземпляр."""
+        # цвет - это внешнее состояние - такие состояния повторяются - храним их отдельно.
+        self._latitude = latitude
+        self._longtitude = longitude
+        self._color = FlyweightFactory.get_color(color_name)
+
+    def __str__(self: PlaceMark) -> str:
+        """Строковое представление объекта."""
+        return f"Координаты {self._latitude}/{self._longtitude}, цвет {self._color}.\n"
+
+
+if __name__ == "__main__":
+    mark_0 = PlaceMark(1.1, 2.2, "red")
+    mark_1 = PlaceMark(3.3, 4.4, "red")
+    mark_2 = PlaceMark(5.5, 6.6, "green")
+
+    print(mark_0, mark_1, mark_2, end="\n")
--- patterns/my_patterns/prototype.py
+++ patterns/my_patterns/prototype.py
@@ -1,5 +1,50 @@
 # coding: utf-8
 """Паттерт прототип."""
+from __future__ import annotations
+
+from copy import copy, deepcopy
+
+
+class Prototype:
+    """Класс позволяющий клонировать свои экземпляры."""
+
+    def __init__(self: Prototype, state: dict) -> None:
+        """Инициализирует экземпляр."""
+        self._state = state
+
+    @property
+    def state(self: Prototype) -> dict:
+        """Возвращает состояние."""
+        return self._state
+
+    def __copy__(self: Prototype) -> Prototype:
+        """Создает поверхностную копию.
+
+        Метод вызывается при вызове метода copy, позволяет определять, что нужно копировать.
+        """
+        clone_object = Prototype(self._state)
+        clone_object.__dict__ = copy(self.__dict__)
+        return clone_object
+
+    def __deepcopy__(self: Prototype, memo: dict = None) -> Prototype:
+        """Создает полную копию экземпляра.
+
+        Метод вызывается при вызове метода deepcopy, позволяет определять, что нужно копировать.
+        """
+        clone_object = Prototype(self._state)
+        clone_object.__dict__ = deepcopy(self.__dict__, memo=memo)
+        return clone_object
+
 
 if __name__ == "__main__":
-    pass
+    state = {"a": 1, "b": 2}
+    first_object = Prototype(state)
+
+    copy_clone = copy(first_object)
+    deepcopy_clone = deepcopy(first_object)
+
+    state["a"] = "7"
+    state["c"] = "10"
+    print(f"first_object.state   {first_object.state}")
+    print(f"copy_clone.state     {copy_clone.state}")
+    print(f"deepcopy_clone.state {deepcopy_clone.state}")
