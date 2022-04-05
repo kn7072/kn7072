9a57cec911eb9ca53da2a117b494a60f680a2134
stepan <kn7072@yandex.ru>
2022-04-05 09:17:16 +0300

    комментарии к словам

--- SQL/cookbook_recipe.sql
+++ SQL/cookbook_recipe.sql
@@ -77,3 +77,31 @@ select ename,job
 from emp
 order by substr(job,length(job)-1);
 
+--2.1 Returning Query Results in a Specified Order
+SELECT ename, job, sal
+FROM emp
+WHERE deptno = 10
+ORDER BY sal ASC;
+
+--2.2 Sorting by Multiple Fields
+SELECT empno, deptno, sal, ename, job
+FROM emp
+ORDER BY deptno, sal DESC;
+
+--2.3 Sorting by Substrings
+SELECT ename, job
+FROM emp
+ORDER BY substr(job, length(job) - 1);
+
+--2.4 Sorting Mixed Alphanumeric Data
+CREATE VIEW V AS
+SELECT ename || ' ' || deptno AS data
+FROM emp;
+
+SELECT * FROM V;
+
+SELECT data
+FROM V
+ORDER BY replace(translate(data, '0123456789', '##########'), '#', '');
+
+SELECT translate('abcdefg', 'bca', ''); --BCA
