873f3e7f110470dc15bc22a628981242e4a46e76
stepan <stepan@yandex.ru>
2024-08-06 17:17:30 +0300

    закрытие файла и удаление

--- qmk/avalanche/v_1/avalanch_2040/v4/keymaps/test_1/keymap.c
+++ qmk/avalanche/v_1/avalanch_2040/v4/keymaps/test_1/keymap.c
@@ -19,21 +19,23 @@
 #define DOWN_RCTL RCTL_T(KC_DOWN)
 #define UP_RSFT RSFT_T(KC_UP)
 #define RIGHT_LALT LALT_T(KC_RIGHT)
+#define MINS_RGUI RGUI_T(KC_MINS)
+#define LSFT_DEL RSFT(KC_DEL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
             KC_GRAVE,  KC_1,    KC_2,     KC_3,       KC_4,    KC_5,                                                     KC_6,   KC_7,     KC_8,     KC_9,    KC_0,       KC_DELETE,
-            KC_TAB,    KC_Q,    KC_W,     KC_E,       KC_R,    KC_T,                                                     KC_Y,   KC_U,     KC_I,     KC_O,    KC_P,       KC_LBRC,
-   KC_ESC,  KC_LSFT,   A_LGUI,  S_LALT,   D_LSFT,     F_LCTL,  KC_G,                                                     KC_H,   J_RCTL,   K_RSFT,   L_LALT,  SCLN_RGUI,  KC_QUOT,       KC_CAPS_LOCK,
+            KC_LSFT,   KC_Q,    KC_W,     KC_E,       KC_R,    KC_T,                                                     KC_Y,   KC_U,     KC_I,     KC_O,    KC_P,       KC_LBRC,
+   KC_ESC,  KC_TAB,   A_LGUI,  S_LALT,   D_LSFT,     F_LCTL,  KC_G,                                                      KC_H,   J_RCTL,   K_RSFT,   L_LALT,  SCLN_RGUI,  KC_QUOT,       KC_CAPS_LOCK,
             KC_LCTL,   KC_Z,    KC_X,     KC_C,       KC_V,    KC_B,    QK_LEADER,  KC_HOME,      KC_END,  KC_BSLS,      KC_N,   KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,    KC_RBRC,
                                 KC_LWIN,  MY_BUTTON,  KC_SPC,  LOWER,   KC_DELETE,                         KC_BACKSPACE, RAISE,  KC_ENT,   KC_PGDN,  KC_PGUP
   ),
 
   [_LOWER] = LAYOUT(
-             _______,   _______,      _______,    KC_MUTE,       KC_MEDIA_STOP, Q_LCTL,                                                   KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT,    KC_MPLY,  _______,
+             _______,   _______,      _______,    KC_MUTE,       KC_MEDIA_STOP, KC_MPLY,                                                  KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT,    Q_LCTL,   LSFT_DEL,
              _______,   KC_1,         KC_2,       KC_3,          KC_4,          KC_5,                                                     KC_6,       KC_7,       KC_8,     KC_9,        KC_0,     KC_LBRC,
-   _______,  _______,   KC_APP_LGUI,  BSLS_LALT,  HOME_LSFT,     END_LCTL,      KC_LPRN,                                                  KC_LEFT,    DOWN_RCTL,  UP_RSFT,  RIGHT_LALT,  KC_MINS,  KC_EQL,    KC_CAPS_LOCK,
+   _______,  _______,   KC_APP_LGUI,  BSLS_LALT,  HOME_LSFT,     END_LCTL,      KC_LPRN,                                                  KC_LEFT,    DOWN_RCTL,  UP_RSFT,  RIGHT_LALT,  MINS_RGUI,  KC_EQL,    KC_CAPS_LOCK,
              _______,   KC_PGDN,      KC_PGUP,    KC_BACKSPACE,  KC_DELETE,     KC_RPRN,    _______,   _______,     _______,   KC_PSCR,   KC_LBRC,    KC_RBRC,    KC_COMM,  KC_DOT,      KC_SLSH,  _______,
                                       _______,    _______,       _______,       _______,    _______,                _______,   _______,   _______,    _______,    _______
   ),
