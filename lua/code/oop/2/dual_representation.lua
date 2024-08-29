d262ed620f310eee0448c2a6a539a66a57e0b196
stepan <stepan@yandex.ru>
2024-08-29 15:10:08 +0300

    dual represettation

---
+++ lua/code/oop/2/dual_representation.lua
@@ -0,0 +1,29 @@
+-- Figure 21.3. Accounts using a dual representation
+local balance = {}
+Account = {}
+
+function Account:withdraw(v)
+    balance[self] = balance[self] - v
+end
+
+function Account:deposit(v)
+    balance[self] = balance[self] + v
+end
+
+function Account:balance()
+    return balance[self]
+end
+
+function Account:new(o)
+    o = o or {}
+    -- create table if user does not provide one
+    setmetatable(o, self)
+    self.__index = self
+    balance[o] = 0
+    -- initial balance
+    return o
+end
+-- We use this class just like any other one:
+a = Account:new{}
+a:deposit(100.00)
+print(a:balance())
