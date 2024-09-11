c3ae4fb357da6ba19ffee618027d8a73b1e726f3
stepan <stepan@yandex.ru>
2024-09-11 13:35:13 +0300

    доработки

--- GO/english/main.go
+++ GO/english/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-	// data, err := os.ReadFile("/home/stapan/GIT/kn7072/ANKI/WORDS/a/accommodation.json")
+	// data, err := os.ReadFile("/home/stepan/GIT/kn7072/ANKI/WORDS/a/accommodation.json")
 	// if err != nil {
 	// 	fmt.Println(data)
 	// }
@@ -19,26 +19,26 @@ func main() {
 	mapExistsWords, mapAdditionalWords := getMapsWordsFromSentence()
 	createFileForAddSentence(mapExistsWords, mapAdditionalWords)
 
-	// var allSentenceObject = generateObjectAllSentence(AW)
-	// v, ok := allSentenceObject["Sadly, she inherited none of her father's musical talent."]
-	// fmt.Println(v, ok)
-	// allSentence(allSentenceObject)
+	var allSentenceObject = generateObjectAllSentence(AW)
+	v, ok := allSentenceObject["Sadly, she inherited none of her father's musical talent."]
+	fmt.Println(v, ok)
+	allSentence(allSentenceObject)
 
-	// pathEng := "/home/stapan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/INFINITIVE_ENG_PRINT.txt"
-	// pathRus := "/home/stapan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/INFINITIVE_RUS_PRINT.txt"
-	// pathResult := "/home/stapan/TEST_VIM/INFINITEVE_RESULT.txt"
+	// pathEng := "/home/stepan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/INFINITIVE_ENG_PRINT.txt"
+	// pathRus := "/home/stepan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/INFINITIVE_RUS_PRINT.txt"
+	// pathResult := "/home/stepan/TEST_VIM/INFINITEVE_RESULT.txt"
 
-	// pathEng := "/home/stapan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/GERUND_ENG_PRINT.txt"
-	// pathRus := "/home/stapan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/GERUND_RUS_PRINT.txt"
-	// pathResult := "/home/stapan/TEST_VIM/GERUND_RESULT.txt"
+	// pathEng := "/home/stepan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/GERUND_ENG_PRINT.txt"
+	// pathRus := "/home/stepan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/GERUND_RUS_PRINT.txt"
+	// pathResult := "/home/stepan/TEST_VIM/GERUND_RESULT.txt"
 
-	// pathEng := "/home/stapan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/PARTICIPLE_ENG_PRINT.txt"
-	// pathRus := "/home/stapan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/PARTICIPLE_RUS_PRINT.txt"
-	// pathResult := "/home/stapan/TEST_VIM/PARTICIPLE_RESULT.txt"
+	// pathEng := "/home/stepan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/PARTICIPLE_ENG_PRINT.txt"
+	// pathRus := "/home/stepan/GIT/kn7072/EnglishSimulate/Project/VERB_NON_FINITE_FORMS/PARTICIPLE_RUS_PRINT.txt"
+	// pathResult := "/home/stepan/TEST_VIM/PARTICIPLE_RESULT.txt"
 
-	pathEng := "/home/stapan/GIT/kn7072/EnglishSimulate/Project/Preposition/EXERCISE_ENG.txt"
-	pathRus := "/home/stapan/GIT/kn7072/EnglishSimulate/Project/Preposition/EXERCISE_RUS.txt"
-	pathResult := "/home/stapan/TEST_VIM/PREPOSIION_RESULT.txt"
+	pathEng := "/home/stepan/GIT/kn7072/EnglishSimulate/Project/Preposition/EXERCISE_ENG.txt"
+	pathRus := "/home/stepan/GIT/kn7072/EnglishSimulate/Project/Preposition/EXERCISE_RUS.txt"
+	pathResult := "/home/stepan/TEST_VIM/PREPOSIION_RESULT.txt"
 
 	convertNotFinteForms(pathEng, pathRus, pathResult)
 }
