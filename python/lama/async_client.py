9a3e16118c52b4c432d632791ecc5d490832eadc
stepan <stepan@yandex.ru>
2024-10-31 13:57:03 +0300

    новые предложения

--- python/lama/async_client.py
+++ python/lama/async_client.py
@@ -1,9 +1,28 @@
-from ollama import AsyncClient
+import asyncio
+from ollama import AsyncClient, Message
 
-from config import HOST, MODEL
+from config import HOST, PORT, MODEL, VerbForm
+from common import Storage
 
-async def chat():
-  message = {'role': 'user', 'content': 'переведи на русский язык - One afternoon, when I was counting on working for an hour or two more, the telephone rang.'}
-  async for part in await AsyncClient(HOST).chat(model=MODEL, messages=[message], stream=True):
-    # print(part['message']['content'], end='', flush=True)
-    return part['message']['content']
+
+async def chat(request: Message) -> str:
+  answer = ""
+  async for part in await AsyncClient(f"{HOST}:{PORT}").chat(model=MODEL, messages=[request], stream=True):
+    content = part["message"]["content"]
+    print(content, end='', flush=True)
+    answer += content
+  return answer
+
+
+sentence_number = "12"
+request_msg = "Найди ошибки в предложении и исправь ошибки если найдешь ошибки -Oleg tells me about Petrov's being sent on a business-trip."
+request: Message  = {'role': 'user', 'content': request_msg}
+answer = asyncio.run(chat(request))
+
+verb_form = VerbForm.GERUND
+storage = Storage(verb_form)
+storage.insert(sentence_number, request_msg, answer)
+storage.save()
+
+print()
+
--- python/lama/main.py
+++ python/lama/main.py
@@ -1,14 +1,10 @@
 # -*- coding:utf-8 -*-
-import asyncio
 from config import VerbForm, request_time_out
-from async_client import chat
 from client import OllamaClient
 from common import InitRequests
 
 init_requests = InitRequests(VerbForm.GERUND, count_request=20, time_out_between_requests=request_time_out)
 lama_client = OllamaClient()
-# res = asyncio.run(chat())
-# res = lama_client.get_response(content="переведи на русский язык - One afternoon, when I was counting on working for an hour or two more, the telephone rang.")
-# print(res)
+
 init_requests.send_request()
 print()
