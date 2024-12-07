28d208af445f010bfbc89b411331387632e03b33
stepan <stepan@yandex.ru>
2024-12-07 12:14:25 +0300

    доработки

--- vim_tmux/nvim/lua/core/plugins.lua
+++ vim_tmux/nvim/lua/core/plugins.lua
@@ -70,7 +70,8 @@ require("lazy").setup({
         dependencies = {
             "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter"
         }
-    }, {'kevinhwang91/nvim-ufo', dependencies = {'kevinhwang91/promise-async'}}
+    }, {'kevinhwang91/nvim-ufo', dependencies = {'kevinhwang91/promise-async'}},
+    {'mmarchini/bpftrace.vim'}
     -- {'window', dir = "~/.local/share/nvim/windows"},
     -- {'name_space', dir = "~/GIT/kn7072/lua/plugins/name_space"}
     -- {'debugger_my', dir = "~/LUA/repo/debugger_my"}
