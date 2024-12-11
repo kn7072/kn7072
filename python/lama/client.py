bb79b884fce6cd2445ce0887e339fbbaa6ef7971
stepan <stepan@yandex.ru>
2024-12-11 11:29:09 +0300

    проверка форматирования

--- python/lama/client.py
+++ python/lama/client.py
@@ -1,19 +1,20 @@
 from ollama import Client
 from config import HOST, MODEL, PORT
 
+
 class OllamaClient:
 
     def __init__(self):
-        self._client = Client(host=f'{HOST}:{PORT}')
-
+        self._client = Client(host=f"{HOST}:{PORT}")
 
     def get_response(self, content: str) -> str:
-        response = self._client.chat(model=MODEL, messages=[
-          {
-            'role': 'user',
-            'content': content,
-          },
-        ])
-        return  response['message']['content']
-
-
+        response = self._client.chat(
+            model=MODEL,
+            messages=[
+                {
+                    "role": "user",
+                    "content": content,
+                },
+            ],
+        )
+        return response["message"]["content"]
