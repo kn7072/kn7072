55e6a9c60e87287e49ae2e91d098ad3079d2ee93
stepan <stepan@yandex.ru>
2024-12-17 18:15:29 +0300

    доработки работы горячих клавиш для русского языка

--- vim_tmux/nvim/lua/core/configs.lua
+++ vim_tmux/nvim/lua/core/configs.lua
@@ -1,44 +1,48 @@
-vim.wo.number = true
-vim.wo.relativenumber = true
+local opt = vim.opt
+local o = vim.o
+local wo = vim.wo
+
+wo.number = true
+wo.relativenumber = true
 
 vim.g.did_load_filetypes = 1
 vim.g.formatoptions = "qrn1"
-vim.opt.showmode = true
-vim.opt.updatetime = 100
-vim.wo.signcolumn = "yes"
-vim.opt.scrolloff = 8
-vim.wo.linebreak = true
-vim.opt.virtualedit = "block"
-vim.opt.undofile = true
-vim.opt.shell = "/bin/fish"
+opt.showmode = true
+opt.updatetime = 100
+wo.signcolumn = "yes"
+opt.scrolloff = 8
+wo.linebreak = true
+opt.virtualedit = "block"
+opt.undofile = true
+opt.shell = "/bin/fish"
 
 -- Mouse
-vim.opt.mouse = "a"
-vim.opt.mousefocus = true
+opt.mouse = "a"
+opt.mousefocus = true
 
 -- Line Numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
 -- Splits
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+opt.splitbelow = true
+opt.splitright = true
 
 -- Clipboard
-vim.opt.clipboard = "unnamedplus"
+opt.clipboard = "unnamedplus"
 
 -- Shorter messages
-vim.opt.shortmess:append("c")
+opt.shortmess:append("c")
 
 -- Indent Settings
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.smartindent = true
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.smartindent = true
 
 -- Fillchars
-vim.opt.fillchars = {
+opt.fillchars = {
 	vert = "│",
 	fold = "⠀",
 	eob = " ", -- suppress ~ at EndOfBuffer
@@ -53,19 +57,19 @@ vim.cmd([[highlight clear LineNr]])
 vim.cmd([[highlight clear SignColumn]])
 
 -- переносить длинные строки
-vim.opt.wrap = true
+opt.wrap = true
 
-vim.opt.foldnestmax = 100
--- vim.opt.foldmethod = "syntax"
-vim.opt.foldcolumn = "1"
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99 -- уровень вложенности фолдов которые будут закрыты, и
+opt.foldnestmax = 100
+-- opt.foldmethod = "syntax"
+opt.foldcolumn = "1"
+o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+o.foldlevelstart = 99 -- уровень вложенности фолдов которые будут закрыты, и
 -- будут автоматически закрываться, если указать 3, то будут закрыты фолты имеющие двух родительских фолдов
-vim.o.foldenable = true
+o.foldenable = true
 
 -- чтобы оставалась последняя строка файла(https://neovim.io/doc/user/options.html#'fixeol')
-vim.opt.fixeol = true
-vim.opt.fixendofline = true
+opt.fixeol = true
+opt.fixendofline = true
 
 vim.env.PATH =
 	string.format("%s:%s", "/home/stepan/.cache/pypoetry/virtualenvs/kn7072-mq-0OsHe-py3.12/bin", vim.env.PATH)
--- vim_tmux/nvim/lua/core/mappings.lua
+++ vim_tmux/nvim/lua/core/mappings.lua
@@ -1,43 +1,42 @@
+local kmap = vim.keymap.set
+local nvim_map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
 vim.g.mapleader = " "
 
 -- NeoTree
-vim.keymap.set('n', '<leader>e', ':Neotree float reveal<CR>')
-vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
+kmap("n", "<leader>e", ":Neotree float reveal<CR>")
+kmap("n", "<leader>o", ":Neotree float git_status<CR>")
 
 -- Navigation
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-vim.keymap.set('n', '<leader>/', ':CommentToggle<CR>')
-
--- Splits
-vim.keymap.set('n', '|', ':vsplit<CR>')
-vim.keymap.set('n', '\\', ':split<CR>')
+kmap("n", "<c-k>", ":wincmd k<CR>")
+kmap("n", "<c-j>", ":wincmd j<CR>")
+kmap("n", "<c-h>", ":wincmd h<CR>")
+kmap("n", "<c-l>", ":wincmd l<CR>")
+kmap("n", "<leader>/", ":CommentToggle<CR>")
 
 -- Other
-vim.keymap.set('n', '<leader>w', ':w<CR>')
-vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
-vim.keymap.set('i', 'jj', '<Esc>')
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+kmap("n", "<leader>w", ":w<CR>")
+kmap("n", "<leader>x", ":BufferLinePickClose<CR>")
+kmap("i", "jj", "<Esc>")
+kmap("n", "<leader>h", ":nohlsearch<CR>")
 
 -- Tabs
-vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
-vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+kmap("n", "<Tab>", ":BufferLineCycleNext<CR>")
+kmap("n", "<s-Tab>", ":BufferLineCyclePrev<CR>")
 
 -- Terminal
-vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float<CR>')
-vim.keymap.set('n', '<leader>th', ':ToggleTerm direction=horizontal<CR>')
-vim.keymap.set('n', '<leader>tv', ':ToggleTerm direction=vertical size=40<CR>')
+kmap("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
+kmap("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>")
+kmap("n", "<leader>tv", ":ToggleTerm direction=vertical size=40<CR>")
 
-vim.keymap.set("n", "gdt", "<cmd>tab split | lua vim.lsp.buf.definition()<CR>",
-               opts)
+kmap("n", "gdt", "<cmd>tab split | lua vim.lsp.buf.definition()<CR>", opts)
 
 -- Toggle relative line number
-vim.keymap.set('n', '<c-l><c-l>', ':set invrelativenumber<CR>')
+kmap("n", "<c-l><c-l>", ":set invrelativenumber<CR>")
 
 -- ctrl + c
-vim.keymap.set('v', '<leader>cc', '"+y<CR>')
+kmap("v", "<leader>cc", '"+y<CR>')
 
 --[[
 spell
@@ -50,13 +49,12 @@ z=z появится полный список
 для исправляния в режиме редактирования
 <c-x><c-s>
 --]]
-vim.keymap.set('n', ']s', function()
-    local spell = vim.wo.spell
-    vim.wo.spell = true
-    vim.opt.spelllang = {"en_us", "ru_ru"}
-    vim.api.nvim_feedkeys(
-        vim.api.nvim_replace_termcodes(']s', true, true, true), 'n', true)
-    -- vim.schedule(function()
-    --     vim.wo.spell = spell
-    -- end)
+kmap("n", "]s", function()
+	local spell = vim.wo.spell
+	vim.wo.spell = true
+	vim.opt.spelllang = { "en_us", "ru_ru" }
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("]s", true, true, true), "n", true)
+	-- vim.schedule(function()
+	--     vim.wo.spell = spell
+	-- end)
 end)
