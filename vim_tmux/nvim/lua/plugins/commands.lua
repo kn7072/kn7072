17a6409cffcb2c34433497027338b0c3ea2f5dfc
stepan <stepan@yandex.ru>
2025-04-23 14:38:02 +0300

    update

--- vim_tmux/nvim/lua/plugins/commands.lua
+++ vim_tmux/nvim/lua/plugins/commands.lua
@@ -1,4 +1,40 @@
 local api = vim.api
+local open = io.open
+
+-- see if the file exists
+function file_exists(file)
+    local f = io.open(file, "rb")
+    if f then
+        f:close()
+    end
+    return f ~= nil
+end
+
+function lines_from(file)
+    if not file_exists(file) then
+        return {}
+    end
+    local lines = {}
+    for line in io.lines(file) do
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+local function read_file(path)
+
+    if not file_exists(path) then
+        return nil
+    end
+
+    local file = open(path, "rb") -- r read mode and b binary mode
+    if not file then
+        return nil
+    end
+    local content = file:read "*a" -- *a or *all reads the whole file
+    file:close()
+    return content
+end
 
 api.nvim_create_user_command('Flake8', function(opts)
     local cmd = "flake8"
@@ -16,8 +52,36 @@ api.nvim_create_user_command('Flake8', function(opts)
 end, {desc = 'First flake8', nargs = 1, bang = true})
 
 api.nvim_create_user_command('Upper', function(opts)
-    print(string.upper(opts.args))
-end, {nargs = 1})
+    local current_word = vim.fn.expand("<cword>")
+    local current_word_upper = string.upper(current_word)
+    vim.cmd("normal! diwi" .. current_word_upper)
+end, {})
+
+api.nvim_create_user_command('SnakeCase', function(opts)
+    local current_word = vim.fn.expand("<cword>")
+    -- change case to snake case
+    local snake_case = current_word:gsub("(%u)", "_%1"):gsub("(%u)",
+                                                             string.lower):gsub(
+                           "^_", "")
+    vim.cmd("normal! diwi" .. snake_case)
+end, {})
+
+api.nvim_create_user_command('ToggleCase', function(opts)
+    local current_word = vim.fn.expand("<cword>")
+    if current_word:match("_") then
+        vim.cmd("CamalCase")
+    else
+        vim.cmd("SnakeCase")
+    end
+end, {})
+
+api.nvim_create_user_command('CamalCase', function(opts)
+    local current_word = vim.fn.expand("<cword>")
+    -- change case to camal case
+    local camal_case = current_word:gsub("_(.)", string.upper):gsub("^(.)",
+                                                                    string.upper)
+    vim.cmd("normal! diwi" .. camal_case)
+end, {})
 
 api.nvim_create_user_command('Tags', function(opts)
     local cursor_word = vim.fn.expand("<cword>")
@@ -41,3 +105,118 @@ api.nvim_create_user_command('Test', function(opts)
     api.nvim_set_hl(0, "FoldColumn",
                     {ctermbg = 70, bg = "#d72323", fg = "#11cbd7", bold = true})
 end, {})
+
+local function split(str, sep)
+    local result = {}
+    local regex = ("([^%s]+)"):format(sep)
+    for each in str:gmatch(regex) do
+        local res, _ = each:gsub("\n", "")
+        table.insert(result, res)
+    end
+    return result
+end
+
+local eng_win_id = -1
+local bufnr = -1
+local data_files_table = {}
+
+local function get_data_file(file_path)
+    local data_file = data_files_table[file_path]
+    if not data_file then
+        local file_content = read_file(file_path)
+        if file_content == nil then
+            error(string.format("file %s not found", file_path))
+        end
+        data_file = file_content
+        data_files_table[file_path] = file_content
+    end
+    return data_file
+end
+
+api.nvim_create_user_command('Eng', function(opts)
+    -- local cmd = string.format(
+    --                 [[' | jq 'to_entries[] | select(.key | test("%s.*"))']],
+    --                 opts.args)
+    -- print(opts.args)
+
+    -- local file_path = "/home/stepan/temp/саша/json"
+    local file_path =
+        "/home/stepan/git_repos/kn7072/ANKI/TelegramBot/all_words.json"
+    local cmd = string.format(
+                    [[jq 'to_entries[] | select(.key | test("^%s.*"))' "%s"]],
+                    opts.args, file_path)
+    -- local cmd = string.format(
+    --                 [[cat %s | jq 'to_entries[] | select(.key | test("%s.*"))']],
+    --                 file_path, opts.args)
+
+    -- print(cmd)
+    -- local file_content = read_file(file_path)
+    -- local file_content = get_data_file(file_path)
+    -- local lines = lines_from(file_path)
+
+    -- if file_content == nil then
+    --     print(string.format("file %s not found", file_path))
+    -- end
+    -- print(file_content)
+    -- print("echo '" .. file_content .. cmd)
+    -- local bufnr = api.nvim_get_current_buf()
+    --
+    --
+    -- local content = vim.fn.system("echo '" .. file_content .. cmd)
+    local content = vim.fn.system(cmd)
+    -- print(content)
+
+    local current_win = api.nvim_get_current_win()
+    local current_win_width = api.nvim_win_get_width(current_win)
+    local current_win_height = api.nvim_win_get_height(current_win)
+
+    if not api.nvim_buf_is_valid(bufnr) then
+        bufnr = api.nvim_create_buf(false, true)
+        -- print(string.format("%s", bufnr))
+        api.nvim_buf_set_name(bufnr, 'English')
+
+    end
+    -- local bufnr = api.nvim_create_buf(false, true)
+    -- print(string.format("%s", bufnr))
+    -- api.nvim_buf_set_name(bufnr, 'English')
+
+    local lines = split(content, "\n")
+    -- for _, line in ipairs(lines) do
+    --     print(line)
+    -- end
+
+    -- lines = {"xxx", "yyyy"}
+    -- api.nvim_buf_set_text(bufnr, 0, 0, 0, 0, lines)
+    local count_lines = api.nvim_buf_line_count(bufnr)
+
+    -- api.nvim_buf_set_lines(bufnr, 0, 0, false, lines) -- если необходимо продолжить писать в тот же буфер
+    api.nvim_buf_set_lines(bufnr, 0, count_lines, false, lines)
+    --
+    -- local window_opt = {
+    --     win = current_win,
+    --     -- split = 'left',
+    --     style = "minimal",
+    --     border = {"╔", "═", "╗", "║", "╝", "═", "╚", "║"},
+    --     -- relative = "editor",
+    --     relative = "win",
+    --     focusable = false, -- ввод текста не возможен
+    --     width = current_win_width,
+    --     height = current_win_height,
+    --     row = 1,
+    --     col = 5
+    -- }
+    if not api.nvim_win_is_valid(eng_win_id) then
+        local window_opt = {win = current_win, split = 'right'} -- , split = 'left'
+        eng_win_id = api.nvim_open_win(bufnr, false, window_opt) -- window_opt
+    end
+
+    api.nvim_set_current_win(eng_win_id)
+
+    -- print(eng_win_id)
+    vim.cmd('redraw')
+
+    -- print(type(content))
+
+    -- print(vim.fn.executable("echo '" .. file_content .. cmd))
+
+end, {desc = "English", nargs = 1})
--- vim_tmux/nvim/lua/plugins/nullls.lua
+++ vim_tmux/nvim/lua/plugins/nullls.lua
@@ -1,14 +1,235 @@
 local null_ls = require("null-ls")
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local helpers = require("null-ls.helpers")
 
+local methods = require("null-ls.methods")
+local log = require("null-ls.logger")
+local u = require("null-ls.utils")
+
+vim.lsp.buf.format({ timeout_ms = 5000 })
+
+local DIAGNOSTICS_ON_SAVE = methods.internal.DIAGNOSTICS_ON_SAVE
+
+local flake8 = {
+    name = "flake8",
+    method = methods.internal.DIAGNOSTICS,
+    filetypes = { "python", "py" },
+    -- null_ls.generator creates an async source
+    -- that spawns the command with the given arguments and options
+    generator = null_ls.generator({
+        command = "flake8",
+        args = {
+                 "--config",
+                 vim.fn.stdpath("config") .. "/plugin_configs/.flake8", "$FILENAME"
+             },
+        -- choose an output format (raw, json, or line)
+        format = "line",
+        check_exit_code = function(code, stderr)
+            local success = code <= 1
+            if not success then
+                -- can be noisy for things that run often (e.g. diagnostics), but can
+                -- be useful for things that run on demand (e.g. formatting)
+                print(stderr)
+            end
+            -- print("success", success)
+
+            return success
+        end,
+        -- use helpers to parse the output from string matchers,
+        -- or parse it manually with a function
+        on_output = helpers.diagnostics.from_patterns({
+             {
+                pattern = [[(%g+):(%d+):(%d+): ([%w%d]+) (.*)]],
+                groups = { "filename", "row", "col", "code", "message" }
+,
+            }        }),
+    }),
+}
+
+
+local no_really = {
+    method = null_ls.methods.DIAGNOSTICS,
+    filetypes = { "markdown", "text" },
+    generator = {
+        fn = function(params)
+            local diagnostics = {}
+            -- sources have access to a params object
+            -- containing info about the current file and editor state
+            for i, line in ipairs(params.content) do
+                local col, end_col = line:find("really")
+                if col and end_col then
+                    -- null-ls fills in undefined positions
+                    -- and converts source diagnostics into the required format
+                    table.insert(diagnostics, {
+                        row = i,
+                        col = col,
+                        end_col = end_col + 1,
+                        source = "no-really",
+                        message = "Don't use 'really!'",
+                        severity = vim.diagnostic.severity.WARN,
+                    })
+                end
+            end
+            return diagnostics
+        end,
+    },
+}
+
+local golang_my = {
+    name = "golangci_lint_x",
+    meta = {
+        url = "https://golangci-lint.run/",
+        description = "A Go linter aggregator.",
+    },
+--    method = DIAGNOSTICS_ON_SAVE,
+        method = null_ls.methods.DIAGNOSTICS,
+
+    filetypes = { "go" },
+    generator_opts = {
+        command = "golangci-lint",
+        to_stdin = true,
+        from_stderr = false,
+        ignore_stderr = true,
+        multiple_files = true,
+        cwd = helpers.cache.by_bufnr(function(params)
+            -- there might be cases when it's needed to setup cwd manually:
+            -- check the golangci-lint docs for relative-path-mode.
+            -- usually projects contain settings in root so this is sane default.
+            return u.root_pattern("go.mod")(params.bufname)
+        end),
+        args = helpers.cache.by_bufnr(function(params)
+            -- params.command respects prefer_local and only_local options
+            local version = vim.system({ params.command, "version" }, { text = true }):wait().stdout
+            -- from observation the version can be either v2.x.x or 2.x.x
+            -- depending on packaging
+            if version and (version:match("version v2") or version:match("version 2")) then
+                return { "run", "--fix=false", "--show-stats=false", "--output.json.path=stdout" }
+            end
+            return { "run", "--fix=false", "--out-format=json" }
+        end),
+        format = "json",
+        check_exit_code = function(code)
+            return code <= 2
+        end,
+        on_output = function(params)
+            local diags = {}
+            if params.output["Report"] and params.output["Report"]["Error"] then
+                log:warn(params.output["Report"]["Error"])
+                return diags
+            end
+            local issues = params.output["Issues"]
+            if type(issues) == "table" then
+                for _, d in ipairs(issues) do
+                    table.insert(diags, {
+                        source = string.format("golangci-lint: %s", d.FromLinter),
+                        row = d.Pos.Line,
+                        col = d.Pos.Column,
+                        message = d.Text,
+                        severity = helpers.diagnostics.severities["warning"],
+                        filename = u.pathelpers.join(params.cwd, d.Pos.Filename),
+                    })
+                end
+            end
+            return diags
+        end,
+    },
+    factory = helpers.generator_factory,
+}
+
+local p_lint = {
+    name = "pylint",
+    meta = {
+        url = "https://github.com/PyCQA/pylint",
+        description = [[
+Pylint is a Python static code analysis tool which looks for programming
+errors, helps enforcing a coding standard, sniffs for code smells and offers
+simple refactoring suggestions.
+
+If you prefer to use the older "message-id" names for these errors (i.e.
+"W0612" instead of "unused-variable"), you can customize pylint's resulting
+diagnostics like so:
+
+```lua
+null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.diagnostics.pylint.with({
+      diagnostics_postprocess = function(diagnostic)
+        diagnostic.code = diagnostic.message_id
+      end,
+    }),
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.black,
+    ...,
+  },
+})
+```
+]],
+    },
+    method = methods.internal.DIAGNOSTICS,
+    filetypes = { "python" },
+    -- generator_opts = {
+    generator = null_ls.generator({
+        command = "pylint",
+        to_stdin = true,
+        args = { "--from-stdin", "$FILENAME", "-f", "json" },
+        format = "json",
+        check_exit_code = function(code)
+            return code ~= 32
+        end,
+        on_output = helpers.diagnostics.from_json({
+            attributes = {
+                row = "line",
+                col = "column",
+                code = "symbol",
+                severity = "type",
+                message = "message",
+                message_id = "message-id",
+                symbol = "symbol",
+                source = "pylint",
+            },
+            severities = {
+                convention = helpers.diagnostics.severities["information"],
+                refactor = helpers.diagnostics.severities["information"],
+            },
+            offsets = {
+                col = 1,
+                end_col = 1,
+            },
+        }),
+        cwd = helpers.cache.by_bufnr(function(params)
+            return u.root_pattern(
+                -- https://pylint.readthedocs.io/en/latest/user_guide/usage/run.html#command-line-options
+                "pylintrc",
+                ".pylintrc",
+                "pyproject.toml",
+                "setup.cfg",
+                "tox.ini"
+            )(params.bufname)
+        end),
+    }),
+}
+--[[
+eslint-d
+https://stackoverflow.com/questions/78108133/issue-with-none-ls-configuration-error-with-eslint-d
+--]]
+--:NullLsInfo
+-- lua print(vim.inspect(require("null-ls").get_sources()))
+--
+-- /home/stepan/.local/share/nvim/mason/bin/pylint --from-stdin /home/stepan/git_repos/kn7072/ANKI/TelegramBot/convert_sentence.py -f json
+--
+-- /home/stepan/.local/share/nvim/mason/bin/flake8 --config /home/stepan/.config/nvim/plugin_configs/.flake8 /home/stepan/git_repos/kn7072/ANKI/TelegramBot/create_file_for_anki_new.py
+
+null_ls.register(no_really)
+null_ls.register(flake8)
+-- null_ls.register(golang_my)
+-- null_ls.register(p_lint)
+-- helpers.make_builtin(golang_my)
+--
+--
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.eslint_d.with({
-            filetypes = {
-                "typescript", "javascript", "typescriptreact", "javascriptreact"
-            }
-        }), null_ls.builtins.formatting.lua_format,
-        null_ls.builtins.diagnostics.eslint_d,
+        -- null_ls.builtins.formatting.lua_format,
+        null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.clang_format.with({
             extra_args = {
                 -- https://clang.llvm.org/docs/ClangFormatStyleOptions.html
@@ -22,19 +243,23 @@ null_ls.setup({
                 string.format("--settings-path=%s", vim.fn.stdpath("config") ..
                                   "/plugin_configs/.isort.cfg")
             }
-        }), null_ls.builtins.diagnostics.flake8.with({
-            extra_args = {
-                "--config",
-                vim.fn.stdpath("config") .. "/plugin_configs/.flake8"
-            }
-        }), null_ls.builtins.diagnostics.shellcheck,
-        null_ls.builtins.formatting.prettierd.with({
-            filetypes = {
-                "html", "json", "jsonc", "yaml", "markdown", "markdown.mdx",
-                "graphql", "handlebars"
-            }
-        })
-    },
+        }), 
+        -- null_ls.builtins.diagnostics.flake8.with({
+        --     extra_args = {
+        --         "--config",
+        --         vim.fn.stdpath("config") .. "/plugin_configs/.flake8"
+        --     }
+        -- }), 
+        -- null_ls.builtins.diagnostics.shellcheck,
+        -- null_ls.builtins.formatting.shfmt.with({
+        --     extra_args = {"-i", "2", "-ci"}, -- , "-ci", "-bn"
+        --     filetypes = {"bash", "sh"}
+        --
+        -- })
+         null_ls.builtins.formatting.prettierd.with({
+        filetypes = { "html", "json", "yaml", "markdown" },
+    }),
+           },
 
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
@@ -54,3 +279,5 @@ null_ls.setup({
         end
     end
 })
+
+
