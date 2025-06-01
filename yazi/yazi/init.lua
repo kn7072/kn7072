0cd27c5758bc0d70dd047860316ff12b984324a9
stepan <stepan@yandex.ru>
2025-06-01 16:59:09 +0300

    изменил просмоторщик

--- yazi/yazi/init.lua
+++ yazi/yazi/init.lua
@@ -7,9 +7,10 @@
 --     ui: Layout system.
 --     ya: Utility API, including functions for system time, debugging, shell commands, etc.
 --     ps: Publish-subscribe system/data distribution service.
+--
+--
 -- Show user/group of files in status bar
 -- https://yazi-rs.github.io/docs/tips#user-group-in-status
---
 Status:children_add(function()
     local h = cx.active.current.hovered
     if h == nil or ya.target_family() ~= "unix" then
@@ -23,3 +24,20 @@ Status:children_add(function()
         " "
     }
 end, 500, Status.RIGHT)
+
+-- https://github.com/MasouShizuka/projects.yazi
+require("projects"):setup({
+    save = {
+        method = "yazi", -- yazi | lua
+        lua_save_path = "" -- comment out to get the default value
+        -- windows: "%APPDATA%/yazi/state/projects.json"
+        -- unix: "~/.local/state/yazi/projects.json"
+    },
+    last = {
+        update_after_save = true,
+        update_after_load = true,
+        load_after_start = false
+    },
+    merge = {quit_after_merge = false},
+    notify = {enable = true, title = "Projects", timeout = 3, level = "info"}
+})
