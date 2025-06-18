4e3e5d0a71c0398700fae9ddebca03653c019b3b
stepan <stepan@yandex.ru>
2025-06-18 17:21:43 +0300

    новые конфиги

--- yazi/yazi/init.lua
+++ yazi/yazi/init.lua
@@ -1,19 +1,7 @@
--- https://yazi-rs.github.io/docs/term   - терминалогия
--- Lua API
---     cx: Synchronous context state.
---     rt: Runtime information, such as user terminal emulator properties and global user preferences.
---     th: Theme system configuration.
---     fs: File system API.
---     ui: Layout system.
---     ya: Utility API, including functions for system time, debugging, shell commands, etc.
---     ps: Publish-subscribe system/data distribution service.
---
---
--- Show user/group of files in status bar
--- https://yazi-rs.github.io/docs/tips#user-group-in-status
+-- https://yazi-rs.github.io/docs/tips
 Status:children_add(function()
     local h = cx.active.current.hovered
-    if h == nil or ya.target_family() ~= "unix" then
+    if not h or ya.target_family() ~= "unix" then
         return ""
     end
 
@@ -25,19 +13,28 @@ Status:children_add(function()
     }
 end, 500, Status.RIGHT)
 
--- https://github.com/MasouShizuka/projects.yazi
-require("projects"):setup({
-    save = {
-        method = "yazi", -- yazi | lua
-        lua_save_path = "" -- comment out to get the default value
-        -- windows: "%APPDATA%/yazi/state/projects.json"
-        -- unix: "~/.local/state/yazi/projects.json"
-    },
-    last = {
-        update_after_save = true,
-        update_after_load = true,
-        load_after_start = false
-    },
-    merge = {quit_after_merge = false},
-    notify = {enable = true, title = "Projects", timeout = 3, level = "info"}
+-- https://yazi-rs.github.io/docs/tips
+Header:children_add(function()
+    if ya.target_family() ~= "unix" then
+        return ""
+    end
+    return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
+
+-- https://github.com/dedukun/bookmarks.yazi
+require("bookmarks"):setup({
+    last_directory = {enable = false, persist = false, mode = "dir"},
+    persist = "all",
+    desc_format = "parent",
+    file_pick_mode = "hover",
+    custom_desc_input = false,
+    notify = {
+        enable = false,
+        timeout = 1,
+        message = {
+            new = "New bookmark '<key>' -> '<folder>'",
+            delete = "Deleted bookmark in '<key>'",
+            delete_all = "Deleted all bookmarks"
+        }
+    }
 })
