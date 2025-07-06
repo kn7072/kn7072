209466da628482b07275348d619694228fba4ef3
stepan <stepan@yandex.ru>
2025-07-06 15:53:32 +0300

    скрипт для поиска слов

---
+++ BASH/search_word.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# set -x
+# путь к файлу со всеми словами
+path_all_words="/home/stepan/git_repos/kn7072/ANKI/TelegramBot/ALL_WORDS.txt"
+# путь к json файлу с примерами
+path_to_all_words_json="/home/stepan/git_repos/kn7072/ANKI/TelegramBot/all_words.json"
+word=$(cat ${path_all_words} | awk -F";" '{print $1}' | fzf --tac --tiebreak=index --height=10)
+# echo "word ${word}"
+command="to_entries[] | select(.key | test(\"^${word}.*\"))"
+jq "${command}" "${path_to_all_words_json}"
+# set +x
