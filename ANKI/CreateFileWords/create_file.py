0225eee516f1b8182748b93c619b79ff18148d2d
stepan <stepan@yandex.ru>
2025-12-03 15:10:10 +0300

    доработки

--- ANKI/CreateFileWords/create_file.py
+++ ANKI/CreateFileWords/create_file.py
@@ -2,26 +2,27 @@
 import json
 import os
 from copy import deepcopy
-from common import get_list_words, get_info_word, get_origin_json, create_file
 
+from common import create_file, get_info_word, get_list_words, get_origin_json
 
 path_script = os.getcwd()
 path_anki = os.path.split(path_script)[0]
 path_words = os.path.join(path_anki, "words.json")
 
 examples_keys = {
-        "comment": [],
-        "translate": "",
-        "transcription": "",
-        "antonyms": [],
-        "mnemonic": [],
-        "examples": [],
-        "example_translate": [],
-        "synonyms": [],
-        "grups": [
-            "all_words",
-        ]
-    }
+    "comment": [],
+    "translate": "",
+    "transcription": "",
+    "antonyms": [],
+    "mnemonic": [],
+    "examples": [],
+    "example_translate": [],
+    "synonyms": [],
+    "grups": [
+        "all_words",
+    ],
+    "stars": 0,
+}
 
 
 all_words = get_origin_json(path_words)
@@ -40,11 +41,16 @@ def create_json_word(word_i):
     copy_temp["examples"] = dict_examples["examples_eng"]
     copy_temp["example_translate"] = dict_examples["examples_rus"]
     temp_dict = {word_i: copy_temp}
-    dir_for_create_json = os.path.join(path_anki, "WORDS", word_i[0].lower(), f"{word_i}.json")
+    dir_for_create_json = os.path.join(
+        path_anki, "WORDS", word_i[0].lower(), f"{word_i}.json"
+    )
     path_notebook = os.path.join(path_anki, "WORDS_NOTEPAD", f"{word_i}.txt")
     if not os.path.isfile(path_notebook):
         contant_head = f"{word_i} {transcription} {translate}"
-        temp_list = [f"\n\n{i[0]}\n{i[1]}\n\n####" for i in zip(dict_examples["examples_eng"], dict_examples["examples_rus"])]
+        temp_list = [
+            f"\n\n{i[0]}\n{i[1]}\n\n####"
+            for i in zip(dict_examples["examples_eng"], dict_examples["examples_rus"])
+        ]
         content_all = contant_head + "".join(temp_list)
         with open(path_notebook, encoding="utf-8", mode="w") as f:
             f.write(content_all)
@@ -68,16 +74,22 @@ def update_word_dict():
 
 list_new_words = get_list_words(path_file_to_save)
 for word_i in list_new_words:
-    if word_i in all_words:
-        # print("Слово уже содержится в списке")
-        pass
-    else:
-        print(f"{word_i}")
-        try:
-            create_json_word(word_i)
-        except Exception as e:
-            print(f"Проблемы {word_i}\n{e}")
+    # if word_i in all_words:
+    #     # print("Слово уже содержится в списке")
+    #     pass
+    # else:
+    #     print(f"{word_i}")
+    #     try:
+    #         create_json_word(word_i)
+    #     except Exception as e:
+    #         print(f"Проблемы {word_i}\n{e}")
+
+    print(f"{word_i}")
+    try:
+        create_json_word(word_i)
+    except Exception as e:
+        print(f"Проблемы {word_i}\n{e}")
+
 
 update_word_dict()
 print(list_new_words)
-
